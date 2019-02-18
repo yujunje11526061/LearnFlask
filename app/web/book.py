7a1182f4f6aeec3fbac060bc138a6ce3474c8cf6
Junjie_Yu <yujunjie@zju.edu.cn>
2019-02-18 22:35:11 +0800

    ?q={}&page={}方式传参，使用WTForms插件验证参数合法性

--- app/web/book.py
+++ app/web/book.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from flask import request, jsonify
-
+from app.forms.book import SearchForm
 from app.web.blueprint import web
 from config import URLISBN
 from httptool import HTTP
@@ -15,20 +15,39 @@ def search(q, page):
     :param q: 普通关键字
     :param page:
     :return:
+    jsonify()函数可将字典封装成response对象
     '''
     r = YushuBook.search_by_isbn(q) if YushuBook.is_ISBN(q) else YushuBook.search_by_key(q)
     return jsonify(r)
+
+# @web.route('/book/search/')
+# def search2():
+#     '''
+#     ?q={}&page={} 方式传参
+#     request上下文对象的args属性（MultiDict）来获取参数
+#     :param q: 普通关键字
+#     :param page:
+#     :return:
+#     '''
+#     q = request.args.get('q')
+#     page = request.args.get('page')
+#     r = YushuBook.search_by_isbn(q) if YushuBook.is_ISBN(q) else YushuBook.search_by_key(q)
+#     return jsonify(r)
 
 @web.route('/book/search/')
 def search2():
     '''
     ?q={}&page={} 方式传参
     request上下文对象的args属性（MultiDict）来获取参数
+    WTForms插件来进行参数验证
     :param q: 普通关键字
     :param page:
     :return:
     '''
-    q = request.args.get('q')
-    page = request.args.get('page')
-    r = YushuBook.search_by_isbn(q) if YushuBook.is_ISBN(q) else YushuBook.search_by_key(q)
-    return jsonify(r)
+    form = SearchForm(request.args)
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data
+        r = YushuBook.search_by_isbn(q) if YushuBook.is_ISBN(q) else YushuBook.search_by_key(q)
+        return jsonify(r)
+    return jsonify({'msg':'parameters are invalid', 'code':400})
