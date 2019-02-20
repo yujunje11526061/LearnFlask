129e93dfece53d34c33f69363bc88aa69cf6a0e0
Junjie_Yu <yujunjie@zju.edu.cn>
2019-02-20 21:41:49 +0800

    使用with语句实现AppContext对象的上下文管理。

--- test/test1.py
+++ test/test1.py
@@ -10,8 +10,15 @@ app = Flask(__name__)
 # Request对象 包装成 RequestContext 请求上下文 对象
 
 # 视图函数中flask会自动帮我们实现_request_ctx_stack， _app_ctx_stack的进出栈，request和current_app是两个代理对象，取两个栈顶。视图函数外得手动实现进出栈。
-ctx = app.app_context()
-ctx.push()
-a = current_app
-print('```````````````',a)
-ctx.pop()
+# ctx = app.app_context()
+# ctx.push()
+# a = current_app
+# print('```````````````',a)
+# ctx.pop()
+
+# 实现了__enter__和__exit__方法的对象（这里是AppContext对象）可以用with语句来管理上下文
+# 在AppContext对象的这两个方法中分别实现了进出栈。
+with app.app_context() as ctx:
+    ctx.push()
+    a = current_app
+    print('```````````````', a)
